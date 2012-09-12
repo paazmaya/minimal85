4b43f584e769fe04935ac4a7f4b8a868848e3bed
Juga Paazmaya <olavic@gmail.com>
2012-09-12 11:31:29 +0300

    Analog feature works better in analog pins...

--- fade_serial/fade_serial.ino
+++ fade_serial/fade_serial.ino
@@ -9,9 +9,9 @@
  This example code is in the public domain.
  */
 
-const int led = 3;           // the pin that the LED is attached to
-const int txPin = 2;
-SoftwareSerial mySerial(4, txPin); // RX, TX
+const int led = 4;           // the pin that the LED is attached to
+const int txPin = 3;
+SoftwareSerial mySerial(2, txPin); // RX, TX
 
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
